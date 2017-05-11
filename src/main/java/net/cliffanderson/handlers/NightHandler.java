f2fb9b727f3b63cbe0fae41f5b9df0db9d0e2541
Cliff Anderson <cliffordmorgananderson@gmail.com>
2017-05-11 18:40:22 -0400

    Additional formatting to night command

--- src/main/java/net/cliffanderson/handlers/NightHandler.java
+++ src/main/java/net/cliffanderson/handlers/NightHandler.java
@@ -29,6 +29,8 @@ public class NightHandler extends CommandHandler {
             name = name + args.get(0);
         }
 
+        System.out.println("\n\nRecent drinks for " + (name.equals("%") ? "everyone" : name.replace("%", "")) + ":");
+
         String sql = "select p.name, d.name, pur.quantity, d.price from purchases pur " +
                 "inner join people p on p.id = pur.personid " +
                 "inner join drinks d on d.id = pur.drinkid " +
@@ -38,21 +40,35 @@ public class NightHandler extends CommandHandler {
 
         ResultSet results = MySQLManager.executeQuery(sql, name);
         double totalCost = 0.0;
+        int totalDrinks = 0;
 
         try {
             while (results.next()) {
                 Person person = BarManager.instance.getPerson(results.getString(1));
                 Drink drink = BarManager.instance.getDrink(results.getString(2));
-                double cost = results.getInt(3) * results.getDouble(4);
+                int quantity = results.getInt(3);
+                double cost = quantity * results.getDouble(4);
 
                 totalCost += cost;
+                totalDrinks += quantity;
 
-                System.out.printf("Name: %s%sDrink: %s%sCost: %s%n", person.getName(), String.format("%" + (15 - person.getName().length()) + "s", " "), drink.getName(), String.format("%" + (15 - drink.getName().length()) + "s", " "),  new DecimalFormat("##0.00").format(cost));
+                // The String.format calls are getting a number of spaces based on the length of the previous argument
+
+                System.out.printf("Name: %s%sDrink: %s%sAmount: %d%sCost: %s%n",
+                        person.getName(),
+                        String.format("%" + (15 - person.getName().length()) + "s", " "),
+                        drink.getName(),
+                        String.format("%" + (15 - drink.getName().length()) + "s", " "),
+                        quantity,
+                        String.format("%" + (15 - String.valueOf(quantity).length()) + "s", " "),
+                        new DecimalFormat("##0.00").format(cost));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        System.out.printf("%nTotal: %10.2f%n", totalCost);
+        //%10.2f: Max of 10 numbers to the left of the decimal and max of 2 numbers to the right
+        System.out.printf("%nTotal cost: %10.2f%n", totalCost);
+        System.out.printf("Total drinks: %5d%n", totalDrinks);
     }
 }
